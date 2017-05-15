c85b707af45807f600525fce9008efcc5d1c1413
Gu Haihong <sea128@163.com>
2017-05-15 11:42:51 +0800

    解决启动列表为空时报错

--- startVM.ps1
+++ startVM.ps1
@@ -2,7 +2,9 @@ $VMName = "centos"
 $PATH = "D:\Program Files (x86)\virtualBox"
 
 $list = & $PATH\VBoxManage list runningvms
-$started = [regex]::matches($list, '\"(.*?)\"') | %{$_.Groups[1].Value}
+if ($list -ne $null){
+    $started = [regex]::matches($list, '\"(.*?)\"') | %{$_.Groups[1].Value}
+}
 
 if ($VMName -in $started) {
     "The machine " + $VMName + " is already started."
