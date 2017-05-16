5fd0e7abbe67ae19bb35322158e83d55cb1321dd
Gu Haihong <sea128@163.com>
2017-05-16 11:41:21 +0800

    支持多机器启动

--- startVM.ps1
+++ startVM.ps1
@@ -1,13 +1,18 @@
-$VMName = "centos"
+$VMName = ,"centos"
 $PATH = "D:\Program Files (x86)\virtualBox"
 
 $list = & $PATH\VBoxManage list runningvms
+
 if ($list -ne $null){
     $started = [regex]::matches($list, '\"(.*?)\"') | %{$_.Groups[1].Value}
 }
 
-if ($VMName -in $started) {
-    "The machine " + $VMName + " is already started."
-}else{
-    & $PATH\VBoxManage startvm $VMName --type headless
+function startvm ($VMName){
+    if ($VMName -in $started) {
+        "The machine '" + $VMName + "' is already started."
+    }else{
+        & $PATH\VBoxManage startvm $VMName --type headless
+    }
 }
+
+$VMName | %{startvm $_}
