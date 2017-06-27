968ba83c6ee13686dfe3b9007c17b82776749a5f
gu haihong <guhaihong128@gmail.com>
2017-06-27 10:23:50 +0800

    git add imap.py

--- imap.py
+++ imap.py
@@ -7,7 +7,6 @@ Mail.select()
 typ, data = Mail.search(None, '(FROM "personal")')
 
 arr = data[0].split()
-
 for num in arr[-10:]:
     typ, data = Mail.fetch(num, '(RFC822)')
     msg = email.message_from_bytes(data[0][1])
